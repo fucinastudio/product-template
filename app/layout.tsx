eb3757a0411847dea1d14ad3e5eb237285727654
kkratterf <kkratterf@gmail.com>
2024-09-12 16:57:19 +0200

    update base config

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,10 @@
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
 
 import { ToastProvider } from '@fucina/ui';
 import { ThemeProvider } from '@/components/theme-provider';
 
 import { sans, brand, logo } from '@/styles/fonts/font';
 import '@/styles/globals.css';
-
-const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'Fucina - Product Template',
